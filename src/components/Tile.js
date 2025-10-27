a615e5e13e26a249eb1dbc0015b637fb919da5fc
Tushar yadav <132874534+ykhushbu183@users.noreply.github.com>
2025-10-27 15:17:45 +0530

    Update Tile.js

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,1 +1,16 @@
+export default function Tile({ src, index, onClick, isSelected }) {
+  return (
+    <img
+      src={src}
+      alt={`Tile ${index}`}
+      style={{
+        border: isSelected ? '3px solid blue' : '1px solid gray',
+        cursor: 'pointer',
+        width: '100%',
+        height: '100%'
+      }}
+      onClick={() => onClick(index)}
+    />
+  );
+}
 
