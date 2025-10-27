cf5eed896876d27c6bd3dfbe678830cf347dbb33
Tushar yadav <132874534+ykhushbu183@users.noreply.github.com>
2025-10-27 21:14:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root");
+createRoot(container).render(<App />);
 
