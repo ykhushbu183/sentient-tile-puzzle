1ff54cb5607bf8400937fc03fd5f5423749fb61e
Tushar yadav <132874534+ykhushbu183@users.noreply.github.com>
2025-10-27 15:18:22 +0530

    Update Timer.js

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,1 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export default function Timer({ isRunning, onTimeUpdate }) {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    if (!isRunning) return;
+    const interval = setInterval(() => {
+      setSeconds(sec => {
+        onTimeUpdate(sec + 1);
+        return sec + 1;
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [isRunning, onTimeUpdate]);
+
+  return <div>Time: {seconds}s</div>;
+}
 
