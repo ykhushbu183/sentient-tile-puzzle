72a39fefdfed3d06c50e624dcf024924cfd460c7
Tushar yadav <132874534+ykhushbu183@users.noreply.github.com>
2025-10-27 15:12:55 +0530

    Update images.js

--- src/data/images.js
+++ src/data/images.js
@@ -1,1 +1,20 @@
+export const images = [
+  '/images/image1.jpg',
+  '/images/image2.jpg',
+  '/images/image3.jpg',
+  '/images/image4.jpg',
+  '/images/image5.jpg',
+  '/images/image6.jpg',
+  '/images/image7.jpg',
+  '/images/image8.jpg',
+  '/images/image9.jpg',
+  '/images/image10.jpg',
+];
+
+
+export const levels = {
+  simple: 2,        // 2x2 grid
+  intermediate: 3,  // 3x3 grid
+  hard: 4           // 4x4 grid
+};
 
